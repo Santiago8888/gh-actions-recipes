d99b7f81e478e4b7da59069d3b0e2ab7a5ef8f09
Santiago8888 <santiago.marti67@gmail.com>
2020-07-19 05:52:33 -0500

    Ring chart working
    Signed-off-by: Santiago8888 <santiago.marti67@gmail.com>

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,9 +1,13 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+import { RingChartComponent } from './ring-chart/ring-chart.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RingChartComponent } from './ring-chart/ring-chart.component';
 
 @NgModule({
   declarations: [
@@ -11,10 +15,14 @@ import { RingChartComponent } from './ring-chart/ring-chart.component';
     RingChartComponent
   ],
   imports: [
+    BrowserModule, 
+    FormsModule,
+    NgxChartsModule,
+    BrowserAnimationsModule,    
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- dashboard/src/app/ring-chart/ring-chart.component.ts
+++ dashboard/src/app/ring-chart/ring-chart.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { single } from './data';
 
 @Component({
-  selector: 'app-ring-chart',
+  selector: 'ring-chart',
   templateUrl: './ring-chart.component.html',
   styleUrls: ['./ring-chart.component.css']
 })
-export class RingChartComponent implements OnInit {
+export class RingChartComponent {
+  single: any[];
+  view: any[] = [700, 400];
 
-  constructor() { }
+  // options
+  gradient: boolean = true;
+  showLegend: boolean = true;
+  showLabels: boolean = true;
+  isDoughnut: boolean = false;
 
-  ngOnInit(): void {
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
+  constructor() {
+    Object.assign(this, { single });
   }
 
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
+  }
 }
