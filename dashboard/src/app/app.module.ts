40eff976405900c7cb5ab498f5c03b3454dca347
Santiago8888 <santiago.marti67@gmail.com>
2020-07-19 03:34:54 -0500

    Minimal ring component implementation.
    Signed-off-by: Santiago8888 <santiago.marti67@gmail.com>

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RingChartComponent } from './ring-chart/ring-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RingChartComponent
   ],
   imports: [
     BrowserModule,
---
+++ dashboard/src/app/ring-chart/ring-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ring-chart',
+  templateUrl: './ring-chart.component.html',
+  styleUrls: ['./ring-chart.component.css']
+})
+export class RingChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
